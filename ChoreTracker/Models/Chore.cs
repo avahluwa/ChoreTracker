0f57ce778c749ef6530b5ddfa481428500a6fbd1
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-10-27 11:09:59 -0500

    Add project files.

---
+++ ChoreTracker/Models/Chore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChoreTracker.Models
+{
+    public class Chore
+    {
+        public long Id { get; set; }
+        public string ChoreName { get; set; }
+        public string nameOfAssigned { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
