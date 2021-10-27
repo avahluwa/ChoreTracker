a52f573d9fb5f651dc064d33db2e5a02e2eb1103
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-10-27 11:29:46 -0500

    adds chorecontext class

---
+++ ChoreTracker/Models/ChoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChoreTracker.Models
+{
+    public class ChoreContext : DbContext
+    {
+        public ChoreContext(DbContextOptions<ChoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Chore> Chores { get; set; }
+    }
+}
